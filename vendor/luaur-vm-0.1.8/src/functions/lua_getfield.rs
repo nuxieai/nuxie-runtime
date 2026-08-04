68061d5258e479e3e2fe30f2d9616b0a7d1a0d8e
Levi McCallum <levi@levimccallum.com>
2026-08-04 06:41:37 -0700

    luau-fork rung1: lua_getfield (VM/src/lapi.cpp) [8f33df91..91caa731]

--- vendor/luaur-vm-0.1.8/src/functions/lua_getfield.rs
+++ vendor/luaur-vm-0.1.8/src/functions/lua_getfield.rs
@@ -16,6 +16,7 @@ use crate::type_aliases::stk_id::StkId;
 #[allow(non_snake_case)]
 pub unsafe fn lua_getfield(L: *mut lua_State, idx: c_int, k: *const c_char) -> c_int {
     lua_c_threadbarrier_lapi(L);
+    crate::ensure_stack!(L, 1);
 
     let t: StkId = index2addr(L, idx);
     api_check!(L, t != luaO_nilobject as StkId);

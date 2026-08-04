889dc1df6fd42c651e4b16a18667161fec2ce5f1
Levi McCallum <levi@levimccallum.com>
2026-08-04 06:38:37 -0700

    luau-fork rung1: lua_pushnumber (VM/src/lapi.cpp) [8f33df91..91caa731]

--- vendor/luaur-vm-0.1.8/src/functions/lua_pushnumber.rs
+++ vendor/luaur-vm-0.1.8/src/functions/lua_pushnumber.rs
@@ -5,6 +5,7 @@ use crate::type_aliases::lua_state::lua_State;
 
 #[export_name = "luaur_lua_pushnumber"]
 pub unsafe fn lua_pushnumber(L: *mut lua_State, n: f64) {
+    crate::ensure_stack!(L, 1);
     setnvalue!((*L).top, n);
     api_incr_top!(L);
 }

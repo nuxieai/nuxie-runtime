a61f3f8853c14d8bc31678f97bf9b197954e7064
Levi McCallum <levi@levimccallum.com>
2026-08-05 12:53:52 -0700

    fix(luaur): repair dangerous dormant fastcalls

--- vendor/luaur-vm-0.1.8/src/functions/luau_f_select.rs
+++ vendor/luaur-vm-0.1.8/src/functions/luau_f_select.rs
@@ -23,11 +23,11 @@ pub unsafe fn luau_f_select(
 ) -> core::ffi::c_int {
     if nparams == 1 && nresults == 1 {
         let func = (*(*l).ci).func;
-        let cl = clvalue!(func);
-        let lcl = core::ptr::addr_of!((*cl).inner.l).cast::<crate::records::closure::LClosure>();
         let p = if FFlag::LuauCIProto.get() {
             (*(*l).ci).p
         } else {
+            let cl = clvalue!(func);
+            let lcl = core::ptr::addr_of!((*cl).inner.l).cast::<crate::records::closure::LClosure>();
             (*lcl).p
         };
         let n = cast_int!((*l).base.offset_from(func) as i32) - (*p).numparams as i32 - 1;
@@ -36,7 +36,7 @@ pub unsafe fn luau_f_select(
             let i = nvalue!(arg0) as i32;
 
             if ((i - 1) as u32) < (n as u32) {
-                setobj_2_s!(l, res, (*l).base.add((n - (i - 1)) as usize - 1));
+                setobj_2_s!(l, res, (*l).base.offset((-n + (i - 1)) as isize));
                 return 1;
             }
         } else if ttisstring!(arg0) && *svalue!(arg0) == b'#' as i8 {

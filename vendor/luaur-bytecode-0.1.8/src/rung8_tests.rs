6b5d3f03706e7603d1bcaed9ac34cba13934e0b5
Levi McCallum <levi@levimccallum.com>
2026-08-04 23:15:15 -0700

    luau-fork rung9: LBC_VERSION_TARGET (Common/include/Luau/Bytecode.h) [decb2d05..86eb0096]

--- vendor/luaur-bytecode-0.1.8/src/rung8_tests.rs
+++ vendor/luaur-bytecode-0.1.8/src/rung8_tests.rs
@@ -13,7 +13,7 @@ fn vector_double_bytecode_version_is_dark_by_default() {
     luaur_common::FFlag::DebugLuauUserDefinedClasses.set(false);
 
     luaur_common::FFlag::LuauCompileEmitVectorDouble.set(false);
-    assert_eq!(BytecodeBuilder::new(None).get_version(), 9);
+    assert_eq!(BytecodeBuilder::new(None).get_version(), 7);
 
     luaur_common::FFlag::LuauCompileEmitVectorDouble.set(true);
     assert_eq!(BytecodeBuilder::new(None).get_version(), 13);
